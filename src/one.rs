b6c333592c334b7590fea36af25cc5e3d925a00f
Beatrix Klebe <beatrixk@hey.com>
2021-12-02 09:27:01 -0500

    revert back to the more elegant one

--- src/one.rs
+++ src/one.rs
@@ -1,21 +1,20 @@
-pub fn first(input: String) -> i32 {
+pub fn first(input: String) -> usize {
     let input = preprocess(input);
     count_increased(1, input)
 }
 
-pub fn second(input: String) -> i32 {
+pub fn second(input: String) -> usize {
     let input = preprocess(input);
     count_increased(3, input)
 }
 
-fn count_increased(width: usize, data: Vec<i32>) -> i32 {
+fn count_increased(width: usize, data: Vec<i32>) -> usize {
     data.windows(width)
         .map(|w| -> i32 { w.iter().sum() })
-        .fold((0, None), |(count, last), w| match last {
-            Some(last) if w > last => (count + 1, Some(w)),
-            _ => (count, Some(w)),
-        })
-        .0
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|w| w[1] > w[0])
+        .count()
 }
 
 fn preprocess(input: String) -> Vec<i32> {
