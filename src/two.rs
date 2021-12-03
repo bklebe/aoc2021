0ebf4fb9e224da859a3aa30580f4f84189b60530
Beatrix Klebe <beatrixk@hey.com>
2021-12-02 19:31:33 -0500

    Extract preprocessing logic

--- src/two.rs
+++ src/two.rs
@@ -1,37 +1,33 @@
+use std::iter::Map;
+use std::str::Lines;
+
 pub fn first(input: &str) -> i32 {
-    let (horizontal, depth) = input
-        .lines()
-        .map(|l| {
-            (
-                &l[..1],
-                l.split(' ').last().unwrap().parse::<i32>().unwrap(),
-            )
-        })
-        .fold((0, 0), |(h, d), (c, v)| match c {
-            "f" => (h + v, d),
-            "d" => (h, d + v),
-            "u" => (h, d - v),
-            _ => (h, d),
-        });
+    let (horizontal, depth) = preprocess(input).fold((0, 0), |(h, d), (c, v)| match c {
+        "f" => (h + v, d),
+        "d" => (h, d + v),
+        "u" => (h, d - v),
+        _ => (h, d),
+    });
 
     horizontal * depth
 }
 
 pub fn second(input: &str) -> i32 {
-    let (horizontal, depth, _) = input
-        .lines()
-        .map(|l| {
-            (
-                &l[..1],
-                l.split(' ').last().unwrap().parse::<i32>().unwrap(),
-            )
-        })
-        .fold((0, 0, 0), |(h, d, a), (c, v)| match c {
-            "f" => (h + v, d + a * v, a),
-            "d" => (h, d, a + v),
-            "u" => (h, d, a - v),
-            _ => (h, d, a),
-        });
+    let (horizontal, depth, _) = preprocess(input).fold((0, 0, 0), |(h, d, a), (c, v)| match c {
+        "f" => (h + v, d + a * v, a),
+        "d" => (h, d, a + v),
+        "u" => (h, d, a - v),
+        _ => (h, d, a),
+    });
 
     horizontal * depth
+}
+
+fn preprocess<'a>(input: &'a str) -> Map<Lines<'_>, fn(&'a str) -> (&str, i32)> {
+    input.lines().map(|l| {
+        (
+            &l[..1],
+            l.split(' ').last().unwrap().parse::<i32>().unwrap(),
+        )
+    })
 }
